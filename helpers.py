a7f58d4a4e1ba26bfbbfe6846cb53f83956eff4a
Erin Rosien <elrosien@gmail.com>
2023-05-23 10:29:09 -0700

    testing

--- helpers.py
+++ helpers.py
@@ -10,24 +10,21 @@ def token_required(our_flask_function):
     def decorated(*args, **kwargs):
         token = None
 
-        print('testing headers', request.headers['x-access-token'])
-
         if 'x-access-token' in request.headers:
             token = request.headers['x-access-token']
         if not token:
             return jsonify({'message': 'Token is missing.'}), 401
         
         try:
-            current_user = User.query.all()
-            # print(token)
-            # print(current_user_token)
-            return current_user
+            current_user_token = User.query.filter_by(token = token).first()
+            print(token)
+            print(current_user_token)
         except:
             owner = User.query.filter_by(token = token).first()
 
             if token != owner.token and secrets.compare_digest(token, owner.token):
                 return jsonify({'message': 'Token is invalid'})
-        return our_flask_function(current_user, *args, **kwargs)
+        return our_flask_function(current_user_token, *args, **kwargs)
     return decorated
 
 class JSONEncoder(json.JSONEncoder):
